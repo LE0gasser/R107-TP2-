fd244f7e071f8b5c2e65368416c8518357049714
e2500616 <leo.gasser682006@gmail.com>
2025-11-13 16:36:37 +0100

    maj

---
+++ tp3/test.py
@@ -0,0 +1,6 @@
+j = 1
+i = 1
+while i <= 4:
+    j+=i
+    i+=1
+    print(f"{i} {j}", end=" ")
---
+++ tp4/exe1.py
@@ -0,0 +1,7 @@
+table=[]
+a = 0
+for i in range(10):
+    a = a + 1
+    table.append(1.2 * a)
+
+print(table)
