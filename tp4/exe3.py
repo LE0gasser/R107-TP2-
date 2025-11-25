cfe7844f733b2280c18ad1e78bfd96aadb01251e
e2500616 <leo.gasser682006@gmail.com>
2025-11-25 15:35:00 +0100

    update

--- tp4/exe3.py
+++ tp4/exe3.py
@@ -2,16 +2,15 @@ nMax=3
 v1=[]
 v2=[]
 n=int(input("quel est votre taille de veteur : "))
-if n < nMax and n >= 1:
+if n <= nMax and n >= 1:
     print("Saisie du premier vecteur :")
     for i in range(n):
-        v1.append(int(input("v1[n] = ")))
+        v1.append(int(input(f"v1[,{i}] = ")))
     print("Saisie du deuxième vecteur :")
     for i in range(n):
-        v2.append(int(input("v2[n] = ")))
+        v2.append(int(input(f"v2[{i}] = ")))
 
     for i in range(n):
-
         tot= v1[i] * v2[i]
     print("Le produit scalaire de v1 par v2 vaut ", tot)
 else:
