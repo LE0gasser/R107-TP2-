2d84477b16e52c5e2d4826166fa931713f8be650
e2500616 <leo.gasser682006@gmail.com>
2025-11-25 15:27:47 +0100

    update

--- tp4/exe2.py
+++ tp4/exe2.py
@@ -1,10 +1,12 @@
 nbe = int(input("Donnez le nombre d'etudiants : "))
-
-moyenne = 0.0;
+dif=0
+moyenne = 0;
 notes = []
 for i in range(nbe):
 
     notes.append(int(input(f"quelles note a eu l'eleves  {i + 1} : ")))
-
 moyenne=sum(notes)/nbe
 print(f"la moyenne de classe est de {moyenne}")
+dif = moyenne - notes
+for i in range(nbe):
+    print({i + 1} | {notes} | {dif})
---
+++ tp4/exe3.py
@@ -0,0 +1,18 @@
+nMax=3
+v1=[]
+v2=[]
+n=int(input("quel est votre taille de veteur : "))
+if n < nMax and n >= 1:
+    print("Saisie du premier vecteur :")
+    for i in range(n):
+        v1.append(int(input("v1[n] = ")))
+    print("Saisie du deuxième vecteur :")
+    for i in range(n):
+        v2.append(int(input("v2[n] = ")))
+
+    for i in range(n):
+
+        tot= v1[i] * v2[i]
+    print("Le produit scalaire de v1 par v2 vaut ", tot)
+else:
+    n=int(input("entrer un nombre entre 1 et 3 : "))
