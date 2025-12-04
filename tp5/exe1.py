38bebbf09938f5237981d49483008c24db18b12d
e2500616 <leo.gasser682006@gmail.com>
2025-12-04 15:35:42 +0100

    maj

--- tp5/exe1.py
+++ tp5/exe1.py
@@ -1,4 +1,4 @@
-nom1=str.capitalize(int(input("quel est votre nom")))
-nom2=str.capitalize(it(input('quel est votre nom')))
+nom1=(str.capitalize(input("quel est votre nom")))
+nom2=(str.capitalize(input("quel est votre nom")))
 print(nom1)
 print(nom2)
