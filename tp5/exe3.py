0fa9ef5cbca83fc8c3a93d25a1b674ff7afed236
e2500616 <leo.gasser682006@gmail.com>
2025-12-04 16:41:45 +0100

    maj

---
+++ tp5/exe3.py
@@ -0,0 +1,12 @@
+chaine = input("Entrez un mot ou une phrase : ")
+
+chaine_epuree = ""
+for i in chaine:
+    if i.isalpha():
+        chaine_epuree += i.lower()
+
+
+if chaine_epuree == chaine_epuree[::-1]:
+    print("C'est un palindrome !")
+else:
+    print("Ce n'est pas un palindrome.")
